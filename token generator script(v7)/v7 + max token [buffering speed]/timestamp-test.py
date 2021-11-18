09b75d0df701d3caeecc946a97222372b4c4a2e8
glogwa68 <clerc.antoine53500@gmail.com>
2021-11-18 12:43:03 +0100

    lastupdate MAJ.
    Maj and adding lastupdate branch.

--- token generator script(v7)/v7 + max token [buffering speed]/timestamp-test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-date = '18/05/2020 - 18:05:12'
-
-# convert string to datetimeformat
-date = datetime.datetime.strptime(date, "%d %m %Y - %H:%M:%S"")
-
-# convert datetime to timestamp
-date = datetime.datetime.timestamp(date)
